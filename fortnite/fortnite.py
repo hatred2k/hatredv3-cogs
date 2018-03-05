6aba14490030a14e21227525fb810e75d1e5a268
hatred2k <noah.outlaw@yahoo.com>
2018-03-05 08:10:10 -0600

    Update fortnite.py

--- fortnite/fortnite.py
+++ fortnite/fortnite.py
@@ -3,6 +3,7 @@ import random
 import json
 
 import discord
+import datetime
 import requests
 from discord.ext import commands
 
@@ -48,17 +49,17 @@ class Fortnite:
             try:
                 new_platform = random.choice(platforms)
                 url = "https://api.fortnitetracker.com/v1/profile/{}/{}".format(new_platform, username)
-                print("new_platform: {}\n1{}".format(new_platform, platforms))
                 data = await self.get_raw_player_data(url)
                 if "error" in data:
                     platforms.remove(new_platform)
                     only_platform = platforms[0]
                     url = "https://api.fortnitetracker.com/v1/profile/{}/{}".format(only_platform, username)
-                    print("only_platform: {}\n2{}".format(only_platform, platforms))
                     data = await self.get_raw_player_data(url)
+                    if "error" in data:
+                        return "Profile could not be found"
                 return data
             except Exception as e:
-                return "Either an error occured or no data could be found:\n```py{}\n```".format(e)
+                return
         else:
             data = requests.request("GET", url, headers=headers)
             return data.json()
@@ -91,40 +92,30 @@ class Fortnite:
     @commands.command()
     async def fortnite(self, ctx: RedContext, username, platform=None):
         """Shows fortnite stats
+
         Defaults to PC"""
         if await self.config.fortnite_api_key() is None:
             await ctx.send("No API token found.\nYou can enter one by using `{}fortniteset token`".format(ctx.prefix))
             return
         else:
             try:
-                temp = await ctx.send("Fetching profile...")
                 author = ctx.author
                 guild = ctx.guild
                 stats = await self.get_fortnite_data(username, platform)
-                username = stats["epicUserHandle"]
-                platform = stats["platformName"]
-                wins = stats["lifeTimeStats"][8]["value"]
-                score = stats["lifeTimeStats"][6]["value"]
-                matches = stats["lifeTimeStats"][7]["value"]
-                kills = stats["lifeTimeStats"][10]["value"]
-                killratio = stats["lifeTimeStats"][11]["value"]
-                time_played = stats["lifeTimeStats"][13]["value"]
-                avg_survival_time = stats["lifeTimeStats"][14]["value"]
-
                 data = discord.Embed(colour=ctx.author.colour)
                 data.set_thumbnail(url=author.avatar_url)
-                data.add_field(name="Username", value=username)
-                data.add_field(name="Platform", value=platform)
-                data.add_field(name="Wins", value=wins)
-                data.add_field(name="Score", value=score)
-                data.add_field(name="Matches Played", value=matches)
-                data.add_field(name="Kills", value=kills)
-                data.add_field(name="K/D Ratio", value=killratio)
-                data.add_field(name="Time Played", value=time_played)
-                data.add_field(name="Average Survival Time", value=avg_survival_time)
+                data.add_field(name="Username", value=stats["epicUserHandle"])
+                data.add_field(name="Platform", value=stats["platformNameLong"])
+                data.add_field(name="Wins", value=stats["lifeTimeStats"][8]["value"])
+                data.add_field(name="Score", value=stats["lifeTimeStats"][6]["value"])
+                data.add_field(name="Matches Played", value=stats["lifeTimeStats"][7]["value"])
+                data.add_field(name="Kills", value=stats["lifeTimeStats"][10]["value"])
+                data.add_field(name="K/D Ratio", value=stats["lifeTimeStats"][11]["value"])
+                data.add_field(name="Time Played", value=stats["lifeTimeStats"][13]["value"])
+                data.add_field(name="Average Survival Time", value=stats["lifeTimeStats"][14]["value"])
                 data.set_footer(text='Retrived from FortniteTracker | {}'.format(datetime.datetime.now().strftime("%A, %B %-d %Y at %-I:%M%p").replace("PM", "pm").replace("AM", "am")), icon_url='https://i.imgur.com/IMjozOI.jpg')
                 data.set_author(name=author.name, icon_url=author.avatar_url)
                 
-                await temp.edit(embed=data)
+                await ctx.send(embed=data)
             except TypeError:
-                await temp.edit(content="That profile could not be found.")
+                await ctx.send("That profile could not be found.")
